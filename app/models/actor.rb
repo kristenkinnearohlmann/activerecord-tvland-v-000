1b07670ca58dc29e95c4923ac419d40e99c02a63
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-03-10 03:15:32 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -10,6 +10,7 @@ class Actor < ActiveRecord::Base
   def list_roles
     roles = []
     self.characters.each do |character|
+      roles << "#{character.name} - #{character.show.name}"
       binding.pry
     end
   end
