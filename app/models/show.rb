a1820666e67c08d8f701999dfd67be2e9a86766f
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-03-10 03:25:56 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,8 +2,4 @@ class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
 
-  def build_network(call_letters)
-    Network.create(call_letters: call_letters)
-  end
-
 end
